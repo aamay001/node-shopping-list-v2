6fb7194299d96cb6f3becd8d170a343e78c7a145
Andy Amaya <aamay001@gmail.com>
2017-08-20 20:06:54 -0700

    recipes post endpoint

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ Recipes.create(
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
+verifyFields = (requiredFields, req) => {
+    for(let i=0; i < requiredFields.length;i++){
+      let field = requiredFields[i];
+      if ( !(field in req.body)) {
+        return { ok : false, which : field };
+      }
+    }
+
+    return { ok : true };
+}
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
@@ -34,14 +45,13 @@ app.get('/shopping-list', (req, res) => {
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+
+  let fieldValidation = verifyFields(requiredFields, req);
+  if ( !fieldValidation.ok ) {
+      let message = `Missing \`${fieldValidation.which}\' in request body`;
       console.error(message);
       return res.status(400).send(message);
-    }
-  }
+  }  
 
   const item = ShoppingList.create(req.body.name, req.body.budget);
   res.status(201).json(item);
@@ -51,6 +61,20 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res) => {
+    const requiredFields = ["name", "ingredients"];
+    let fieldValidation = verifyFields(requiredFields, req);
+
+    if ( !fieldValidation.ok ) {
+        let message = `Missing \`${fieldValidation.which}\' in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+
+    let item = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(200).json(item);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
